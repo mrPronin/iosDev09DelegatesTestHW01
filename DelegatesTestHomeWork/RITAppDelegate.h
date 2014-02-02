3a0b558b271e48c3e3798108a715073b51360372
Pronin Alexander <pronin.alx@gmail.com>
2014-02-02 19:59:45 +0200

    Initial Commit

---
+++ DelegatesTestHomeWork/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  DelegatesTestHomeWork
+//
+//  Created by Aleksandr Pronin on 02.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DelegatesTestHomeWork/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DelegatesTestHomeWork
+//
+//  Created by Aleksandr Pronin on 02.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RITAppDelegate class]));
+    }
+}
